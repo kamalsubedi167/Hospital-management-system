6936624c5033c9ce22fedc6229638749c55b1426
kamalsubedi167 <kamalsubedi2058@gmail.com>
2025-04-19 21:39:01 +0545

    Added search functionality to patient list

--- core/views.py
+++ core/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render, redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Patient
 from .forms import PatientForm
+from django.db.models import Q
 
 def home(request):
     return render(request, 'home.html')
 
 @login_required
 def patient_list(request):
-    patients = Patient.objects.all()
+    query = request.GET.get('q')
+    if query:
+        patients = Patient.objects.filter(
+            Q(first_name__icontains=query) |
+            Q(last_name__icontains=query) |
+            Q(patient_id__icontains=query)
+        )
+    else:
+        patients = Patient.objects.all()
     return render(request, 'patient_list.html', {'patients': patients})
 
 @login_required
@@ -21,6 +30,7 @@ def add_patient(request):
     else:
         form = PatientForm()
     return render(request, 'add_patient.html', {'form': form})
+
 @login_required
 def edit_patient(request, id):
     patient = get_object_or_404(Patient, id=id)
@@ -32,6 +42,7 @@ def edit_patient(request, id):
     else:
         form = PatientForm(instance=patient)
     return render(request, 'edit_patient.html', {'form': form, 'patient': patient})
+
 @login_required
 def delete_patient(request, id):
     patient = get_object_or_404(Patient, id=id)
